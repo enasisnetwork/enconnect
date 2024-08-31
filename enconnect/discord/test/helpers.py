90bce6204b6fd68b6f8f429058cfc97b824ad12a
Robert Harris <30379153+enrh@users.noreply.github.com>
2024-08-31 19:10:39 +0000

    Add new chat client classes (#29)

---
+++ enconnect/discord/test/helpers.py
@@ -0,0 +1,191 @@
+"""
+Functions and routines associated with Enasis Network Remote Connect.
+
+This file is part of Enasis Network software eco-system. Distribution
+is permitted, for more information consult the project license file.
+"""
+
+
+
+from json import dumps
+from ssl import SSLContext
+from time import sleep as block_sleep
+from typing import Iterator
+from typing import Optional
+from typing import Protocol
+from typing import overload
+from unittest.mock import MagicMock
+from unittest.mock import Mock
+
+from encommon.types import DictStrAny
+
+from httpx import Response
+
+from pytest import fixture
+
+from pytest_mock import MockerFixture
+
+from respx import MockRouter
+
+
+
+EVENTS = Optional[list[DictStrAny]]
+
+SOCKET = tuple[
+    SSLContext,
+    MagicMock]
+
+
+
+class DSCClientSocket(Protocol):
+    """
+    Typing protocol which the developer does not understand.
+    """
+
+    @overload
+    def __call__(
+        self,
+        rvents: EVENTS,
+    ) -> SOCKET:
+        ...  # NOCVR
+
+    @overload
+    def __call__(
+        self,
+    ) -> SOCKET:
+        ...  # NOCVR
+
+    def __call__(
+        self,
+        rvents: EVENTS = None,
+    ) -> SOCKET:
+        """
+        Construct the instance for use in the downstream tests.
+
+        :param rvents: Raw events for playback from the server.
+        """
+        ...  # NOCVR
+
+
+
+RVENTS: list[DictStrAny] = [
+
+    {'t': 'READY',
+     's': 1,
+     'op': 0,
+     'd': {
+         'heartbeat_interval': 1000,
+         'session_id': 'mocked',
+         'user': {
+             'username': 'dscbot',
+             'id': '10101010'}}},
+
+    {'op': 7, 'd': None},
+
+    {'t': 'READY',
+     's': 1,
+     'op': 0,
+     'd': {
+         'heartbeat_interval': 1000,
+         'session_id': 'mocked',
+         'user': {
+             'username': 'dscbot',
+             'id': '10101010'}}},
+
+    {'t': None,
+     's': None,
+     'op': 11,
+     'd': None}]
+
+
+
+@fixture
+def client_dscsock(  # noqa: CFQ004
+    mocker: MockerFixture,
+    respx_mock: MockRouter,
+) -> DSCClientSocket:
+    """
+    Construct the instance for use in the downstream tests.
+
+    :param mocker: Object for mocking the Python routines.
+    :param respx_mock: Object for mocking request operation.
+    :returns: Newly constructed instance of related class.
+    """
+
+    content = dumps({
+        'url': 'mocked'})
+
+    (respx_mock
+     .get(
+         'https://discord.com'
+         '/api/v10/gateway')
+     .mock(side_effect=[
+         Response(
+             status_code=200,
+             content=content),
+         Response(
+             status_code=200,
+             content=content)]))
+
+
+    socmod = mocker.patch(
+        ('enconnect.discord'
+         '.client.connect'),
+        autospec=True)
+
+
+    def _encode(
+        resps: list[DictStrAny],
+    ) -> list[bytes]:
+
+        return [
+            dumps(x).encode('utf-8')
+            for x in resps]
+
+
+    def _delayed(
+        events: list[bytes],
+    ) -> Iterator[bytes]:
+
+        for event in events:
+
+            block_sleep(0.25)
+
+            yield event
+
+
+    def _factory(
+        rvents: list[DictStrAny],
+    ) -> MagicMock:
+
+        effect = _delayed(
+            _encode(rvents))
+
+        socket = MagicMock()
+
+        socket.send = Mock()
+
+        socket.recv = Mock(
+            side_effect=effect)
+
+        socket.close = Mock()
+
+        return socket
+
+
+    def _fixture(
+        rvents: EVENTS = None,
+    ) -> SOCKET:
+
+        if rvents is None:
+            rvents = []  # NOCVR
+
+        socket = _factory(
+            RVENTS + rvents)
+
+        socmod.return_value = socket
+
+        return (socmod, socket)
+
+
+    return _fixture
---
+++ enconnect/irc/models.py
@@ -0,0 +1,199 @@
+"""
+Functions and routines associated with Enasis Network Remote Connect.
+
+This file is part of Enasis Network software eco-system. Distribution
+is permitted, for more information consult the project license file.
+"""
+
+
+
+from re import compile
+from re import match as re_match
+from typing import Annotated
+from typing import Literal
+from typing import Optional
+
+from encommon.types import BaseModel
+from encommon.types import DictStrAny
+
+from pydantic import Field
+
+
+
+EVENT = compile(
+    r'^(?::(?P<prefix>[^\s]+)\s)?'
+    r'(?P<command>[A-Z0-9]{3,})\s'
+    r'(?P<params>.+)$')
+
+KINDS = Literal[
+    'event',
+    'chanmsg',
+    'privmsg']
+
+MESSAGE = [
+    'chanmsg',
+    'privmsg']
+
+
+
+class ClientEvent(BaseModel, extra='ignore'):
+    """
+    Contains information returned from the upstream server.
+    """
+
+    prefix: Annotated[
+        Optional[str],
+        Field(None,
+              description='Prefix or origin information',
+              min_length=1)]
+
+    command: Annotated[
+        str,
+        Field(...,
+              description='Code or command for the event',
+              min_length=1)]
+
+    params: Annotated[
+        Optional[str],
+        Field(None,
+              description='Event or command parameters')]
+
+
+    def __init__(
+        self,
+        /,
+        event: str,
+    ) -> None:
+        """
+        Initialize instance for class using provided parameters.
+        """
+
+        data: DictStrAny = {}
+
+        match = re_match(
+            EVENT, event)
+
+        assert match is not None
+
+
+        prefix = (
+            match
+            .group('prefix'))
+
+        command = (
+            match
+            .group('command'))
+
+        params = (
+            match
+            .group('params'))
+
+
+        if prefix is not None:
+            data['prefix'] = (
+                prefix.strip())
+
+        if command is not None:
+            data['command'] = (
+                command.strip())
+
+        if params is not None:
+            data['params'] = (
+                params.strip())
+
+
+        super().__init__(**data)
+
+
+    @property
+    def kind(
+        self,
+    ) -> KINDS:
+        """
+        Return the value for the attribute from class instance.
+
+        :returns: Value for the attribute from class instance.
+        """
+
+        command = self.command
+        params = self.params
+
+
+        if command == 'PRIVMSG':
+
+            assert params is not None
+
+            prefix = params[0][:1]
+
+            return (
+                'chanmsg'
+                if prefix in '#&+!'
+                else 'privmsg')
+
+
+        return 'event'
+
+
+    @property
+    def author(
+        self,
+    ) -> Optional[str]:
+        """
+        Return the value for the attribute from class instance.
+
+        :returns: Value for the attribute from class instance.
+        """
+
+        kind = self.kind
+        prefix = self.prefix
+
+        if kind not in MESSAGE:
+            return None
+
+        return prefix
+
+
+    @property
+    def recipient(
+        self,
+    ) -> Optional[str]:
+        """
+        Return the value for the attribute from class instance.
+
+        :returns: Value for the attribute from class instance.
+        """
+
+        kind = self.kind
+        params = self.params
+
+        if (kind not in MESSAGE
+                or not params):
+            return None
+
+        split = params.split(' ')
+
+        return split[0]
+
+
+    @property
+    def message(
+        self,
+    ) -> Optional[str]:
+        """
+        Return the value for the attribute from class instance.
+
+        :returns: Value for the attribute from class instance.
+        """
+
+        kind = self.kind
+        params = self.params
+
+        if (kind not in MESSAGE
+                or not params):
+            return None
+
+        message = (
+            params
+            .split(':', maxsplit=1))
+
+        return message[1]
